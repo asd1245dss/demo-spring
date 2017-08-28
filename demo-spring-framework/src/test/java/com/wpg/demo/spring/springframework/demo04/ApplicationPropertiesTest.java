301febe100a693b5416a86e762d7e610292a993b
Summer Lee <1163525902@qq.com>
2017-08-28 11:40:11 +0800

    @TestPropertySource could not only override property source but also define extra properties

--- demo-spring-framework/src/test/java/com/wpg/demo/spring/springframework/demo04/ApplicationPropertiesTest.java
+++ demo-spring-framework/src/test/java/com/wpg/demo/spring/springframework/demo04/ApplicationPropertiesTest.java
@@ -4,26 +4,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
+ *
+ * @TestPropertySource could override @PropertySource value
+ *
  * @author ChangWei Li
  * @version 2017-08-28 11:24
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationProperties.class)
-@TestPropertySource(properties = {"app.name=test-demo-spring"})
+@TestPropertySource(properties = {"app.name=test-demo-spring", "test=ok"})
 @Slf4j
 public class ApplicationPropertiesTest {
 
     @Autowired
     ApplicationProperties applicationProperties;
+
+    @Autowired
+    Environment env;
 
     @Test
     public void read_env_properties() {
@@ -32,6 +37,15 @@ public class ApplicationPropertiesTest {
         log.info("application => {}", applicationProperties);
 
         assertEquals(expectedAppName, applicationProperties.getName());
+    }
+
+    @Test
+    public void read_extra_test_env_properties() {
+        String expectedExtraProperties = "ok";
+
+        log.info("application => {}", env);
+
+        assertEquals(expectedExtraProperties, env.getProperty("test"));
     }
 
 }
