065a15df1c69827bf533c780051a729b70264708
Summer Lee <1163525902@qq.com>
2017-08-28 14:05:02 +0800

    add spring boot module test log home and source log home has different root folder(test root folder is current project but source root folder is the parent root)

---
+++ demo-spring-framework/src/main/java/com/wpg/demo/spring/springframework/demo05/WebApplication.java
@@ -0,0 +1,20 @@
+package com.wpg.demo.spring.springframework.demo05;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author ChangWei Li
+ * @version 2017-08-28 13:46
+ */
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
