bb3a6b942018caec42ea2086bd80123b969449e2
Summer Lee <1163525902@qq.com>
2017-09-11 13:39:53 +0800

    test docker integration with spring boot and maven

--- demo-spring-boot/src/main/java/com/wpg/demo/spring/demo/spring/boot/DemoSpringBootApplication.java
+++ demo-spring-boot/src/main/java/com/wpg/demo/spring/demo/spring/boot/DemoSpringBootApplication.java
@@ -2,6 +2,8 @@ package com.wpg.demo.spring.demo.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoSpringBootApplication {
@@ -9,4 +11,15 @@ public class DemoSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootApplication.class, args);
 	}
+}
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+
+    @RequestMapping("/msg")
+    String hello() {
+        return "Hello Docker !";
+    }
+
 }
