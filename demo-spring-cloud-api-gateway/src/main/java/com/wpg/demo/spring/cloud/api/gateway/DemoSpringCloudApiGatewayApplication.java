73684f2d6a4c00f69b80ac8c3e3aa2172e3d0d95
Summer Lee <1163525902@qq.com>
2017-09-20 10:46:16 +0800

    use @EnableZuulProxy instead of the @EnableZuulServer to make the route effect

--- demo-spring-cloud-api-gateway/src/main/java/com/wpg/demo/spring/cloud/api/gateway/DemoSpringCloudApiGatewayApplication.java
+++ demo-spring-cloud-api-gateway/src/main/java/com/wpg/demo/spring/cloud/api/gateway/DemoSpringCloudApiGatewayApplication.java
@@ -3,11 +3,11 @@ package com.wpg.demo.spring.cloud.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulServer
+@EnableZuulProxy
 public class DemoSpringCloudApiGatewayApplication {
 
 	public static void main(String[] args) {
