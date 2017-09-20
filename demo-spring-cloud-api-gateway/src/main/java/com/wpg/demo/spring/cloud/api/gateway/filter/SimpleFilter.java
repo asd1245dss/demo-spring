2f5c3c51002b466178d1d2e6e6986a125cd2f26e
Summer Lee <1163525902@qq.com>
2017-09-20 09:38:26 +0800

    update cloud api gateway

---
+++ demo-spring-cloud-api-gateway/src/main/java/com/wpg/demo/spring/cloud/api/gateway/filter/SimpleFilter.java
@@ -0,0 +1,10 @@
+package com.wpg.demo.spring.cloud.api.gateway.filter;
+
+import org.springframework.cloud.netflix.zuul.ZuulFilterInitializer;
+
+/**
+ * @author ChangWei Li
+ * @version 2017-09-20 09:13
+ */
+public class SimpleFilter  {
+}
